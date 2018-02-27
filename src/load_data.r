f1b40e59a40de2759651f9cf308ed50893eefa6d
Ubuntu <ubuntu@ip-172-31-24-125.us-west-2.compute.internal>
2018-02-27 23:35:38 +0000

    update

---
+++ src/load_data.r
@@ -0,0 +1,14 @@
+is_mlbench_installed <- require("mlbench")
+if (!is_mlbench_installed) {
+    install.packages("mlbench")
+    library(mlbench)
+}
+
+data(BostonHousing2)
+
+# indexing dataframe using Census tract code
+rownames(BostonHousing2) <- BostonHousing2$tract
+
+# removing attributes in updated dataset BostonHousing2 and not in original BostonHousing.
+# I removed tract because I made it my datasets row names above and do not want it as a feature.
+BostonHousing2 <- subset(BostonHousing2, select = -c(cmedv, town, lon, lat, tract))
